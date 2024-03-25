9be3345100ea67680fa0e619d7dc854365204761
Yavor Petkov <YavorPetkov@students.softuni.bg>
2024-03-25 21:27:32 +0200

    Added error component

--- shop-app/src/app/app-routing.module.ts
+++ shop-app/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './core/error/error.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: ErrorComponent },
+
 ];
 
 @NgModule({
