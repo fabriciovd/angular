b7c1972da5763d10a142f7f441bc6ef129e738d8
Yavor Petkov <YavorPetkov@students.softuni.bg>
2024-03-26 21:56:24 +0200

    Added welcome message component

--- shop-app/src/app/shared/shared.module.ts
+++ shop-app/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WelcomeMsgComponent
+  ],
   imports: [
     CommonModule
   ]
