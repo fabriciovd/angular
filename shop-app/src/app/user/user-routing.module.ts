4a7c83dbbd7067d157e554c3aac5da6aaf5a34ac
Yavor Petkov <YavorPetkov@students.softuni.bg>
2024-03-31 11:38:12 +0300

    Edited routes for the user component.

--- shop-app/src/app/user/user-routing.module.ts
+++ shop-app/src/app/user/user-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
-import { ProfileComponent } from '../profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
